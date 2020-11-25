f5ca22e5437276a49ac2dabafa7467d3bbb58d68
James <james@crocker.us>
2020-11-25 10:46:23 +0000

    fixed kill all, modified launchall

--- killall.sh
+++ killall.sh
@@ -1,1 +1,1 @@
-tmux list-sessions | awk 'BEGIN{FS=":"}{print $1}' | xargs -n 1 tmux kill-session -t
+ls build/logic_servers | xargs -n 1 tmux kill-session -t
--- launchall.sh
+++ launchall.sh
@@ -1,16 +1,18 @@
-if [-z "$RABBITMQ_SECRET"]; then
+#!/usr/bin/env bash
+if [ -z "$RABBITMQ_SECRET" ];
+then
+	echo "launching in Development Mode"
+	for pls in build/logic_servers/*; do
+		IFS='_' read -ra split <<< "$(basename $pls)"
+		tmux new-session -d -s "$(basename $pls)" "$(echo $pls) -host 127.0.0.1 -port 5672 -queue st`echo $split` -user psserver -pass fizzbuzz"
+	done
+else
 	echo "launching assets in Production Mode"
 	for pls in build/logic_servers/*
        	do
 		IFS='_' read -ra split <<< "$(basename $pls)"
 		RABBITMQ_SECRET=$RABBITMQ_SECRET tmux new-session -d -s "$(basename $pls)" "$(echo $pls) -host 127.0.0.1 -port 5672 -queue st`echo $split` -user psserver -pass `echo $RABBITMQ_SECRET`"
 	done
-; else
-	echo "launching in Development Mode"
-	for pls in build/logic_servers/*; do
-		IFS='_' read -ra split <<< "$(basename $pls)"
-		tmux new-session -d -s "$(basename $pls)" "$(echo $pls) -host 127.0.0.1 -port 5672 -queue st`echo $split` -user psserver -pass fizzbuzz"
-	done
-; fi
+fi
 
 
